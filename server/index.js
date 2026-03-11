adf87044e01bac06e904bb2a89546a50578d5023
Kithupa G <gkithupa@gmail.com>
2026-03-11 14:55:37 +0530

    Fixed index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //create a todo
 
-app.post("/todo", async(req, res) => {
+app.post("/api/todo", async(req, res) => {
     try{
         const { description } = req.body;
         const newTodo = await pool.query(
@@ -27,7 +27,7 @@ app.post("/todo", async(req, res) => {
 
 //get all todo
 
-app.get("/todos", async(req , res) => {
+app.get("/api/todos", async(req , res) => {
     try {
         const allTodos = await pool.query("SELECT * FROM todo")
         res.json(allTodos.rows);
@@ -38,7 +38,7 @@ app.get("/todos", async(req , res) => {
 
 // get todo
 
-app.get("/todos/:id", async (req, res) => {
+app.get("/api/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
@@ -52,7 +52,7 @@ app.get("/todos/:id", async (req, res) => {
 
 // update a todo
 
-app.put("/todos/:id", async(req, res) => {
+app.put("/api/todos/:id", async(req, res) => {
    try {
         const {id} = req.params;
         const {description} = req.body;
@@ -67,7 +67,7 @@ app.put("/todos/:id", async(req, res) => {
 
 // delete a todo
 
-app.delete("/todos/:id", async (req, res) => {
+app.delete("/api/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
