ccde856b8f861766f5d8b371c94c29465df80002
Kithupa G <gkithupa@gmail.com>
2026-03-29 14:57:04 +0530

    Removed non free tier elligible instance type

--- infra/main.tf
+++ infra/main.tf
@@ -84,7 +84,7 @@ module "eks" {
     eks_managed_node_groups = {
         dev_nodes = {
         ami_type       = "AL2023_x86_64_STANDARD"
-        instance_types = ["t3.medium", "t3.large"]
+        instance_types = ["t3.medium"]
         min_size       = 1
         max_size       = 3
         desired_size   = 2
